28ce9d642406a7d68996952a3e8d86ff0e949345
m.koshelev <m.koshelev@andersenlab.com>
2022-07-25 18:01:59 +0700

    add simple functional to send tg messages

--- main.go
+++ main.go
@@ -4,14 +4,20 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	tgbotapi "github.com/Syfaro/telegram-bot-api"
 )
 
 type Configuration struct {
-	Chatid int64
-	Token  string
+	Chatid     int64
+	Token      string
+	ListenPort int
+}
+
+type recv_message_str struct {
+	Message string
 }
 
 func getargs() Configuration {
@@ -26,18 +32,44 @@ func getargs() Configuration {
 	return (configuration)
 }
 
+func SendTgMessage(tgbot *tgbotapi.BotAPI) func(rw http.ResponseWriter, request *http.Request) {
+	if tgbot == nil {
+		panic("nil tgbot session!")
+	}
+	return func(rw http.ResponseWriter, request *http.Request) {
+		if request.Method == "POST" {
+			conf := getargs()
+			decoder := json.NewDecoder(request.Body)
+
+			var req recv_message_str
+
+			err := decoder.Decode(&req)
+			msg := tgbotapi.NewMessage(conf.Chatid, req.Message)
+			tgbot.Send(msg)
+			if err != nil {
+				panic(err)
+			}
+			rw.WriteHeader(http.StatusOK)
+		} else {
+			rw.WriteHeader(http.StatusMethodNotAllowed)
+			rw.Write([]byte("Method not allowed."))
+		}
+	}
+}
+
 func main() {
 	conf := getargs()
 	bot, err := tgbotapi.NewBotAPI(conf.Token)
 	if err != nil {
 		log.Panic(err)
 	}
-
 	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-	msg := tgbotapi.NewMessage(conf.Chatid, "Start notify system")
-	bot.Send(msg)
+
+	// Addr: ":" + fmt.Sprint(HttpPort)
+	http.HandleFunc("/", SendTgMessage(bot))
+	go http.ListenAndServe(":"+fmt.Sprint(conf.ListenPort), nil)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
